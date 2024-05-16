7a8a81e738088e47007303c4c6043b66586cf8ca
rakhymzhan <Rahimzhan_T@mail.ru>
2024-05-16 23:01:37 +0500

    push

--- app/index.tsx
+++ app/index.tsx
@@ -4,17 +4,7 @@ import {View} from "react-native";
 
 export default function Index() {
     return (
-        <Stack>
-            <View style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '100%'
-            }}>
-                <Link href="/SignInPage">Sign in</Link>
-            </View>
-        </Stack>
+        <SignUp/>
     );
 }
 
